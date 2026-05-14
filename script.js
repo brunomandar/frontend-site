6d3aeed532c9325a28cebee63eca31907b451d3c
brunomandar <152931733+brunomandar@users.noreply.github.com>
2026-05-14 14:11:42 -0300

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function executarPython() {
-  fetch("https://backend-python-9k0p.onrender.com/")
+  fetch("https://backend-python-9k0p.onrender.com/executar")
     .then(resp => resp.json())
     .then(data => {
       document.getElementById("resultado").innerText = data.resultado;
