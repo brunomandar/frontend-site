a4eb9bd0909a962e8b5c43039c68e142e984505a
brunomandar <152931733+brunomandar@users.noreply.github.com>
2026-05-14 15:14:01 -0300

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function executarPython() {
 
-  fetch("https://backend-python-9k0p.onrender.com")
-    .then(response => response.json())
+  fetch("https://backend-python-9k0p.onrender.com/executar")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Erro na API");
+      }
+      return response.json();
+    })
     .then(data => {
-      document.getElementById("resultado").innerText = data.status;
+      document.getElementById("resultado").innerText = data.resultado;
     })
     .catch(error => {
       document.getElementById("resultado").innerText = "Erro ao conectar API";
